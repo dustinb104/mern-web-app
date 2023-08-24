de2defd60575251e0f22d1b69856714f97f25a04
Dustin Bell <dustinb104@gmail.com>
2023-08-23 19:01:28 -0500

    Rest API for Posts

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -2,11 +2,15 @@
 // syntax when we are making calls to our database.
 const asyncHandler = require('express-async-handler')
 
+const Post = require('../models/postModel')
+
 // @desc Get Posts
 // @route GET /api/posts
 // @access Private
 const getPosts = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Get posts'})
+    const posts = await Post.find()
+
+    res.status(200).json(posts)
 })
 
 // @desc Set Post
@@ -18,21 +22,45 @@ const setPost = asyncHandler(async (req, res) => {
         throw new Error('Please add a text field')
     }
 
-    res.status(200).json({message: 'Set post'})
+    const post = await Post.create({
+        text: req.body.text
+    })
+
+    res.status(200).json(post)
 })
 
 // @desc Update Post
 // @route PUT /api/posts/:id
 // @access Private
 const updatePost = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Update post ${req.params.id}`})
+
+    const post = await Post.findById(req.params.id)
+
+    if(!post){
+        res.status(400)
+        throw new Error('Post not found')
+    }
+
+    const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+    res.status(200).json(updatedPost)
 })
 
 // @desc Delete Post
 // @route DELETE /api/posts/:id
 // @access Private
 const deletePost = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Delete post ${req.params.id}`})
+
+    const post = await Post.findById(req.params.id)
+
+    if(!post){
+        res.status(400)
+        throw new Error('Post not found')
+    }
+
+    await post.deleteOne()
+
+    res.status(200).json({id: req.params.id})
 })
 
 module.exports = {
