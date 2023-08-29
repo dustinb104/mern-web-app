80700cf5d9aee19e3182b82481928b7be98634a0
Dustin Bell <dustinb104@gmail.com>
2023-08-28 21:11:03 -0500

    Finished backend api, has full CRUD functionality

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -3,12 +3,13 @@
 const asyncHandler = require('express-async-handler')
 
 const Post = require('../models/postModel')
+const User = require('../models/userModel')
 
 // @desc Get Posts
 // @route GET /api/posts
 // @access Private
 const getPosts = asyncHandler(async (req, res) => {
-    const posts = await Post.find()
+    const posts = await Post.find({ user: req.user.id})
 
     res.status(200).json(posts)
 })
@@ -23,7 +24,8 @@ const setPost = asyncHandler(async (req, res) => {
     }
 
     const post = await Post.create({
-        text: req.body.text
+        text: req.body.text,
+        user: req.user.id
     })
 
     res.status(200).json(post)
@@ -41,6 +43,20 @@ const updatePost = asyncHandler(async (req, res) => {
         throw new Error('Post not found')
     }
 
+    const user = await User.findById(req.user.id)
+
+    // check if user exists
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    // make sure the logged in user matches the post user
+    if(post.user.toString() != user.id){
+        res.status(401)
+        throw new Error('User not authorized')
+    }
+
     const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, {new: true})
 
     res.status(200).json(updatedPost)
@@ -56,6 +72,20 @@ const deletePost = asyncHandler(async (req, res) => {
     if(!post){
         res.status(400)
         throw new Error('Post not found')
+    }
+
+    const user = await User.findById(req.user.id)
+
+    // check if user exists
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    // make sure the logged in user matches the post user
+    if(post.user.toString() != user.id){
+        res.status(401)
+        throw new Error('User not authorized')
     }
 
     await post.deleteOne()
--- backend/routes/postsRoutes.js
+++ backend/routes/postsRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const {getPosts, setPost, updatePost, deletePost} = require('../controllers/postController')
+const {protect} = require('../middleware/authMiddleware')
 
 // router.get('/', getPosts)
 // router.post('/', setPost)
-router.route('/').get(getPosts).post(setPost)
+router.route('/').get(protect, getPosts).post(protect, setPost)
 
 // router.put('/:id', updatePost)
 // router.delete('/:id',  deletePost)
-router.route('/:id').put(updatePost).delete(deletePost)
+router.route('/:id').put(protect, updatePost).delete(protect, deletePost)
 
 module.exports = router
